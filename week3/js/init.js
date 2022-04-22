a03de93c263d60e6fa72df76801ffac7837ac55b
antonyblin <46633905+antonyblin@users.noreply.github.com>
2022-04-22 03:11:47 -0700

    Add files via upload

---
+++ week3/js/init.js
@@ -0,0 +1,33 @@
+// declare the map
+const map = L.map('the_map').setView([34.0709,-118.444], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// use our marker functions
+addMarker(32.63055,-117.09326,'Tacos El Gordo','yummy taco')
+addMarker(37.42216,-121.91751,'E-Noodle','yummy noodle')
+addMarker(34.04610,-118.25070,'Bohemian House of Espresso + Chai','yummy coffee')
+addMarker(38.55791,-121.47787,'Dim Sum House','yummy dim sum')
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.circleMarker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    createButtons(lat,lng,title);
+    return message
+}
+
+// create function for buttons
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    document.getElementById("contents").appendChild(newButton); //this adds the button to our page.
+}
